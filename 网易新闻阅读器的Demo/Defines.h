9f44d064d469d544af12c973f8daa1dcd6c8878f
HQ今日磨墨 <HQ_Dev@163.com>
2015-12-02 00:06:00 +0800

    整理完了目录结构

---
+++ 网易新闻阅读器的Demo/Defines.h
@@ -0,0 +1,13 @@
+//
+//  Defines.h
+//  网易新闻阅读器的Demo
+//
+//  Created by apple on 15/12/1.
+//  Copyright © 2015年 Archy. All rights reserved.
+//
+
+#ifndef Defines_h
+#define Defines_h
+
+
+#endif /* Defines_h */
---
+++ 网易新闻阅读器的Demo/HomePage.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  网易新闻阅读器的Demo
+//
+//  Created by apple on 15/12/1.
+//  Copyright © 2015年 Archy. All rights reserved.
+//
+
+#import "HomePage.h"
+
+@implementation HomePage
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
